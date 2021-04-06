39f9cb3548e81d8609727baddd5e08c02c6d3abe
zhangchi05 <zhangchi05@kuaishou.com>
2021-04-06 10:02:25 +0800

    feat: 忽略.git目录

--- commands/reportCommand/reportCommand.js
+++ commands/reportCommand/reportCommand.js
@@ -1,11 +1,14 @@
 const Stat = require('./Stat');
 const path = require('path');
 const logger = require('./../../libs/logger');
+const defaultIngoreDirs  = ['.git'];
+
 module.exports = {
     exec({ reportDirPath, srcDirPath, reportTemplate }) {
-        logger.info(`reportCommand exec reportDirPath:${reportDirPath} srcDirPath:${srcDirPath}`)
+        // logger.info(`reportCommand exec reportDirPath:${reportDirPath} srcDirPath:${srcDirPath}`)
+        
         const statInstance = new Stat(srcDirPath, {
-            ingoreDirs: [],
+            ingoreDirs: [].concat(defaultIngoreDirs),
             ingoreFiles: [],
             ignoreComments: false, // 是否注释代码注释
             reportTemplate: reportTemplate,
