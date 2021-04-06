1082b683d642e6be222300ab5bb48d920bd395eb
zhangchi05 <zhangchi05@kuaishou.com>
2021-04-06 10:25:58 +0800

    feat:默认忽略package-lock 或者 yarn.lock

--- commands/reportCommand/reportCommand.js
+++ commands/reportCommand/reportCommand.js
@@ -1,21 +1,21 @@
-const Stat = require('./Stat');
-const path = require('path');
-const logger = require('./../../libs/logger');
-const defaultIngoreDirs  = ['.git','node_modules'];
+const Stat = require("./Stat");
+const path = require("path");
+const logger = require("./../../libs/logger");
+const defaultIngoreDirs = [".git", "node_modules"];
+const defaultIngoreFileds = ["package-lock.json"];
 
 module.exports = {
-    exec({ reportDirPath, srcDirPath, reportTemplate }) {
-        // logger.info(`reportCommand exec reportDirPath:${reportDirPath} srcDirPath:${srcDirPath}`)
-        
-        const statInstance = new Stat(srcDirPath, {
-            ingoreDirs: [].concat(defaultIngoreDirs),
-            ingoreFiles: [],
-            ignoreComments: false, // 是否注释代码注释
-            reportTemplate: reportTemplate,
-            reportDirPath: reportDirPath
-        }).boot();
+  exec({ reportDirPath, srcDirPath, reportTemplate }) {
+    // logger.info(`reportCommand exec reportDirPath:${reportDirPath} srcDirPath:${srcDirPath}`)
 
-        statInstance.generateReport();
+    const statInstance = new Stat(srcDirPath, {
+      ingoreDirs: [].concat(defaultIngoreDirs),
+      ingoreFiles: [].concat(defaultIngoreFileds),
+      ignoreComments: false, // 是否注释代码注释
+      reportTemplate: reportTemplate,
+      reportDirPath: reportDirPath,
+    }).boot();
 
-    }
-}
+    statInstance.generateReport();
+  },
+};
